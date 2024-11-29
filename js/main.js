44b095fc7cf7d1a951ab3566e3058e6ac8bc2bb2
signsofdidier <signsofdidier@gmail.com>
2024-11-29 13:18:02 +0100

    update

--- js/main.js
+++ js/main.js
@@ -67,3 +67,68 @@ const initSlider = (containerClass, thumbMinId, thumbMaxId, rangeClass, displayI
 initSlider('.slider-container.desktop', 'thumbMinDesktop', 'thumbMaxDesktop', '.range', 'priceDisplayDesktop');
 initSlider('.slider-container.mobile', 'thumbMinMobile', 'thumbMaxMobile', '.range', 'priceDisplayMobile');
 
+/*navbar on scroll*/
+const navbar = document.querySelector('.navbar'); // De navbar
+const navUl = document.querySelector('#nav-ul'); // De navbar
+const navbarTinyImg = document.querySelector('#navbar-tiny-img'); // De navbar
+const placeholder = document.createElement('div'); // Een element dat als placeholder dient
+
+// Voeg de placeholder in direct boven de navbar
+navbar.parentNode.insertBefore(placeholder, navbar);
+
+// Event listener voor scrollen
+window.addEventListener('scroll', function () {
+    if (window.scrollY > navbar.offsetTop) {
+        // navbar fixed bij het scrollen
+        navbar.classList.add('fixed-top'); // Maak de navbar vast
+        navUl.classList.add('navbar-small');
+        navbarTinyImg.classList.add('navbar-tiny-img-small');
+        placeholder.style.display = 'block'; // Activeer de placeholder
+        placeholder.style.height = `${navbar.offsetHeight}px`; // Geef de placeholder dezelfde hoogte als de navbar
+    } else {
+        // Terug naar boven en verwijder de classes
+        navbar.classList.remove('fixed-top'); // Verwijder de vaste positie
+        navUl.classList.remove('navbar-small');
+        navbarTinyImg.classList.remove('navbar-tiny-img-small');
+        placeholder.style.display = 'none'; // Verberg de placeholder
+        placeholder.style.height = '0'; // Reset de hoogte
+    }
+});
+
+/*NAVBAR navbar-active links*/
+const navLinks = document.querySelectorAll('.nav-link');
+
+/*navbar-active links*/
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', function () {
+        navLinks.forEach(navLink => {
+            navLink.classList.remove('navbar-active');
+        });
+        navLink.classList.add('navbar-active');
+    });
+});
+
+/*SLUIT MODAL WANNEER JE OP DE SEARCH KNOP KLIKT*/
+const searchButton = document.querySelector('#exampleModal .btn');
+
+searchButton.addEventListener('click', function () {
+    // Zoek de modal en sluit deze
+    const modalElement = document.getElementById('exampleModal');
+    const modalInstance = bootstrap.Modal.getInstance(modalElement);
+    if (modalInstance) {
+        modalInstance.hide();
+    }
+});
+
+/*COPYRIGHT jaar aanpassing*/
+const copyrightYearElement = document.getElementById('copyrightYear');
+// Haal het huidige jaar op
+const currentYear = new Date().getFullYear();
+// Stel het huidige jaar in als de tekst van het element
+copyrightYearElement.textContent = currentYear;
+
+
+
+
+
+
